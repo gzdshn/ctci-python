be7090d5f5b85d6223aca5816290ed17b8ac5059
gsahin <gsahin@usc.edu>
2018-06-04 10:01:39 -0700

    q1.1 - additional set implementation

--- chapter1/q1.py
+++ chapter1/q1.py
@@ -1,22 +1,21 @@
 ## CTCI - Arrays and Strings Question 1
 ## Assumes true is returned when a string is empty
-## Uses lists, checks whether character already exists - O(n^2) worst case
+## Uses sets, set look-up time O(1) in average case in python
+## Addition for max number of characters check
+from sets import Set
 
+MaxNumChar = 128
 def uniqueAll(st):
-	uniqueList = []
-	for character in st:
-		found = False
-		for elmt in uniqueList:
-			if elmt==character:
-				found = True
-				break
-		if not found:
-			uniqueList.append(character)
-
-	if len(st)==len(uniqueList):
-		return True
-	else:
+	if len(st)>MaxNumChar:
 		return False
+	uniqueSet = Set()
+	for character in st:
+		if character in uniqueSet:
+			return False
+		else:
+			uniqueSet.add(character)
+
+	return True
 
 print uniqueAll("")
 print uniqueAll("appalachia")
